8f6b8acf2bde062aa045181410e9342e82ed9aa7
janjunjon <jun.mr.children@gmail.com>
2022-07-19 17:39:55 +0900

    add configuration text of fnl #40

--- src/models/MSMs.py
+++ src/models/MSMs.py
@@ -5,12 +5,39 @@ class MSMs:
         self.nc = nc
         self.lat, self.lon = self.getLatLon(nc)
         self.r1h = self.getRain(nc)
+        self.u, self.v = self.getWind(nc)
+        self.rh = self.getRelativeHumidity(nc)
+        self.temp = self.getTemperature(nc)
+        self.w = self.getVerticalVelocity(nc)
+        self.z = self.getGeopotentialHeight(nc)
         pass
 
     def getLatLon(nc):
-        lat, lon = netcdf._returnConvertedLatLon(nc)
+        lat = nc.variables['lat']
+        lon = nc.variables['lon']
         return lat, lon
 
     def getRain(nc):
-        r1h = netcdf.convertMSMsTo3hours(nc) 
+        r1h = nc.variables['r1h']
         return r1h
+
+    def getWind(nc):
+        u = nc.variables['u']
+        v = nc.variables['v']
+        return u, v
+    
+    def getRelativeHumidity(nc):
+        rh = nc.variables['rh']
+        return rh
+
+    def getTemperature(nc):
+        temp = nc.variables['temp']
+        return temp
+
+    def getVerticalVelocity(nc):
+        w = nc.variables['w']
+        return w
+
+    def getGeopotentialHeight(nc):
+        z = nc.variables['z']
+        return z
