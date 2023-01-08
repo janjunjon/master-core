81284801a8cbf7451e8b861fdc34d3039186b3db
janjunjon <jun.mr.children@gmail.com>
2023-01-08 18:00:12 +0900

    [fix] modify execution of mmodel5 #106

--- src/ML/Execution/v3/v3Pattern5.py
+++ src/ML/Execution/v3/v3Pattern5.py
@@ -1,3 +1,5 @@
+from External.Slack import Slack
+
 # from netCDF.v3.HeavyRainCases.LinearRegression import Model5 as HeavyLinearModel5
 # instance = HeavyLinearModel5()
 # instance.create()
@@ -18,15 +20,21 @@ instance = RainLinearModel5()
 instance.create()
 instance.correct()
 
+Slack.notification(f'correct finished.: {type(instance)}')
+
 from netCDF.v3.Rain.SDGRegressor import Model5 as RainSDGModel5
 instance = RainSDGModel5()
 instance.create()
 instance.correct()
 
+Slack.notification(f'correct finished.: {type(instance)}')
+
 from netCDF.v3.Rain.NonLinearSDGRegressor import Model5 as RainNonLinearModel5
 instance = RainNonLinearModel5()
 instance.create()
 instance.correct()
+
+Slack.notification(f'correct finished.: {type(instance)}')
 
 # SVR
 
@@ -35,7 +43,11 @@ instance = HeavySVRModel5()
 instance.create()
 instance.correct()
 
+Slack.notification(f'correct finished.: {type(instance)}')
+
 from netCDF.v3.Rain.SVR import Model5 as RainSVRModel5
 instance = RainSVRModel5()
 instance.create()
 instance.correct()
+
+Slack.notification(f'correct finished.: {type(instance)}')
