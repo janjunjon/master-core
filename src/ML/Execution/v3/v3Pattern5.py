bbc40407099195298e056a62e564a88d5ad825fd
janjunjon <jun.mr.children@gmail.com>
2023-01-08 17:51:03 +0900

    [add] class for model5 execution #106

---
+++ src/ML/Execution/v3/v3Pattern5.py
@@ -0,0 +1,41 @@
+# from netCDF.v3.HeavyRainCases.LinearRegression import Model5 as HeavyLinearModel5
+# instance = HeavyLinearModel5()
+# instance.create()
+# instance.correct()
+
+# from netCDF.v3.HeavyRainCases.SDGRegressor import Model5 as HeavySDGModel5
+# instance = HeavySDGModel5()
+# instance.create()
+# instance.correct()
+
+# from netCDF.v3.HeavyRainCases.NonLinearSDGRegressor import Model5 as HeavyNonLinearModel5
+# instance = HeavyNonLinearModel5()
+# instance.create()
+# instance.correct()
+
+from netCDF.v3.Rain.LinearRegression import Model5 as RainLinearModel5
+instance = RainLinearModel5()
+instance.create()
+instance.correct()
+
+from netCDF.v3.Rain.SDGRegressor import Model5 as RainSDGModel5
+instance = RainSDGModel5()
+instance.create()
+instance.correct()
+
+from netCDF.v3.Rain.NonLinearSDGRegressor import Model5 as RainNonLinearModel5
+instance = RainNonLinearModel5()
+instance.create()
+instance.correct()
+
+# SVR
+
+from netCDF.v3.HeavyRainCases.SVR import Model5 as HeavySVRModel5
+instance = HeavySVRModel5()
+instance.create()
+instance.correct()
+
+from netCDF.v3.Rain.SVR import Model5 as RainSVRModel5
+instance = RainSVRModel5()
+instance.create()
+instance.correct()
